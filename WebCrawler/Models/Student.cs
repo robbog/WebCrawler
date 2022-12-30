43abbd4405807b92bc67f52c4fcf755d2aacc755
RobertBogacz <RobertBogacz@RBOGACZ01>
2022-12-30 23:54:05 +0100

    Add project files.

---
+++ WebCrawler/Models/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Models
+{
+    public class Student
+    {
+        private string imie;
+        private string nazwisko;
+
+        //tworzenie gettera i settera klasycznie:
+        public string GetImie()
+        {
+            return imie;
+        }
+
+        public void SetImie(string imie)
+        {
+            if(imie == null)
+            {
+                throw new System.Exception("Imie nie moze byc nullem");
+            };
+            this.imie = imie;
+        }
+
+        //tworzenie gettera i settera poprzez Auto-Property prop:
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //tworzenie gettera i settera poprzez Full-Property propfull:
+        private string _lastName;
+
+        public string LastNameFull
+        {
+            get { return _lastName; }
+            set { 
+                if(value == null)
+                {
+                    throw new ArgumentException("Lastname cannot be null");
+                }
+                   _lastName = value;
+            }
+        }
+
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        //konstruktor
+        public Student (string email)
+        {
+            this.Email = email;
+        }
+
+
+
+    }
+}
---
+++ WebCrawler/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebCrawler.Models;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static async Task Main(string[] args) 
+            //wprowadzone przez async, klasycznie ta metoda wygląda tak: static void Main(string[] args)
+        {
+            /*
+             * PODSTAWY JĘZYKA:
+
+            //iteracja:
+            int count = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                count += i;
+            }
+            Console.WriteLine("Wynik " + count);
+
+            //użycie gettera i settera (klasycznego) z klasy Student
+            Student st = new Student();  //po utworzeniu konstruktora tutaj brakuje parametru
+            st.SetImie("Tomek");
+            Console.WriteLine(st.GetImie());
+
+            //użycie gettera i settera (property) z klasy Student
+            //Student st = new Student();
+            st.LastNameFull = "Bogacz";
+            Console.WriteLine(st.LastNameFull);
+
+            //użycie konstruktora
+            Student st2 = new Student("rb@gmail.com");
+            st2.FirstName = "Magda"
+
+            */
+
+            string websiteUrl = "https://www.pja.edu.pl/";
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(websiteUrl); //metoda asynchroniczna oczekująca na odpowiedź. Aby zadziałała metoda main musi wyglądać tak: static async Task Main(string[] args)
+
+            string content = await response.Content.ReadAsStringAsync(); //pobieranie zawartości strony
+            Console.WriteLine(content);
+
+            //wyszukiwanie adresów email
+            MatchCollection email = Regex.Matches(content, "[a-zA-Z]+[@]+[a-zA-Z.]+");
+            foreach(var r in email)
+            {
+                Console.WriteLine(r);
+            }
+
+            //wyszukiwanie numerów telefonu
+            MatchCollection telnumber = Regex.Matches(content, "[+]+[0-9]+[0-9]+[^a-zA-Z]");
+            foreach (var r in telnumber)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
